9c51e96a16aec9d41cae87012282e2bbc1daaad3
ahn0min <73797608+Chickegg@users.noreply.github.com>
2023-09-17 09:55:18 +0900

    chore: pre-merged complie

--- dist/index.js
+++ dist/index.js
@@ -1,4 +1,4 @@
 "use strict";
-Object.defineProperty(exports, "__esModule", {value: true});
+Object.defineProperty(exports, "__esModule", { value: true });
 exports.index = void 0;
 exports.index = "'i'm Index";
